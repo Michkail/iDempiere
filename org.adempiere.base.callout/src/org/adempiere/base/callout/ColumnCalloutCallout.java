a257ee2da9484ca3b6bad82d13eef17d0a51a372
rusli_sru <rusli@sinarrodautama.co.id>
2021-09-27 16:02:06 +0700

    slurp

---
+++ org.adempiere.base.callout/src/org/adempiere/base/callout/ColumnCalloutCallout.java
@@ -0,0 +1,16 @@
+package org.adempiere.base.callout;
+
+import java.util.Properties;
+
+@Override
+public class ColumnCalloutCallout implements ColumnCallout{
+    publicGridField mField, Object value, Object oldValue) {
+        System.out.println(ctx+" - "+WindowNo+" - "+mTab+" - "+mField+" - "+value+" - "+oldValue);
+        return null;
+
+        System.ou.println(etx+" - ")
+    };
+    public static void main(String []args){
+
+    };
+}
